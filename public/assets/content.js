455f6f2d98b29ef0db3ba1df64582105b90eedeb
Nicolás Contreras <nikitastrike@icloud.com>
2023-05-07 21:22:49 -0500

    Removed # on querySelector

--- public/assets/content.js
+++ public/assets/content.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const scene = document.querySelector("a-scene");
   const arSystem = scene.systems["mindar-image-system"];
 
-  const [target_1] = [document.querySelector("#plane-1")];
+  const [target_1] = [document.querySelector("plane-1")];
 
   target_1.addEventListener("targetFound", (event) => {
     alert("target found");
