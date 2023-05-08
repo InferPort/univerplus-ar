ad981064d9d26016e994f65c177ffe14a142c30e
Nicolás Contreras <nikitastrike@icloud.com>
2023-05-07 21:47:20 -0500

    Added audio for first plane

--- public/assets/content.js
+++ public/assets/content.js
@@ -1,16 +1,16 @@
 const playAudio = (audio) => {
   var audioData = new Audio(audio);
   audioData.play();
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   const scene = document.querySelector("a-scene");
   const arSystem = scene.systems["mindar-image-system"];
 
-  const [target_1] = [document.querySelector("plane-1")];
+  const [target_1] = [document.querySelector("#plane_1")];
 
   target_1.addEventListener("targetFound", (event) => {
-    alert("target found");
+    playAudio("https://api.inferport.com/univerplus/audio/plane_1.mp3");
   });
 
   arSystem.stop();
