c607c4d9aed2a5a72e36589a8b5a3b631e341b22
Nicolás Contreras <nikitastrike@icloud.com>
2023-05-07 21:18:10 -0500

    Demo alert when first object is detected

--- public/assets/content.js
+++ public/assets/content.js
@@ -1,7 +1,17 @@
+const playAudio = (audio) => {
+  var audioData = new Audio(audio);
+  audioData.play();
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const scene = document.querySelector("a-scene");
   const arSystem = scene.systems["mindar-image-system"];
+
+  const [target_1] = [document.querySelector("#plane-1")];
+
+  target_1.addEventListener("targetFound", (event) => {
+    alert("target found");
+  });
 
   arSystem.stop();
 });
