145e13808517f19951d3dcd0f45436bcad142e30
Nicolás Contreras <nikitastrike@icloud.com>
2023-05-11 13:00:31 -0500

    Changed scale + removed additional info

--- public/assets/content.js
+++ public/assets/content.js
@@ -30,5 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  arSystem.stop();
+  scene.addEventListener("arReady", (event) => {
+    console.log("arReady");
+  });
 });
